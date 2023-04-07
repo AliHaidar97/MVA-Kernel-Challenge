a2ae498afca64620b4f78661e881f11b6719f330
ali haidar <alihaidar.97@hotmail.com>
2023-04-07 19:37:28 +0200

    fix svm

--- models.py
+++ models.py
@@ -1,8 +1,17 @@
 from scipy import optimize
 from scipy.linalg import cho_factor, cho_solve
 import numpy as np
+import cvxopt
+import cvxopt.solvers
+from sklearn.base import BaseEstimator, ClassifierMixin
 
-class KernelSVC:
+def to_binary(y):
+    return ((y + 1) / 2).astype(int)
+
+def sigmoid(x):
+    return 1/(1 + np.exp(-x))
+
+class KernelSVC(BaseEstimator, ClassifierMixin):
     
     def __init__(self, C, epsilon = 1e-3):
         self.type = 'non-linear'
@@ -14,47 +23,35 @@ class KernelSVC:
        
     
     def fit(self, K_train, y):
-       #### You might define here any variable needed for the rest of the code
+        
+        y = np.array(y)
+       
+        #### You might define here any variable needed for the rest of the code
         N = len(y)
         
-        # Lagrange dual problem
-        def loss(alpha):
-            #'''--------------dual loss ------------------ '''
-            return (1/2)*((np.diag(y)@alpha.reshape(-1,1)).T@K_train@(np.diag(y)@alpha.reshape(-1,1))) - np.sum(alpha)
+        K_train += 1   
         
-        # Partial derivate of Ld on alpha
-        def grad_loss(alpha):
-            # '''----------------partial derivative of the dual loss wrt alpha -----------------'''
-            return np.diag(y)@K_train@np.diag(y)@alpha.reshape(-1,1) - 1
+        # Set up quadratic programming problem
+        P = cvxopt.matrix(np.outer(y, y) * K_train)
+        q = cvxopt.matrix(-1 * np.ones(N))
+        G = cvxopt.matrix(np.vstack((-1 * np.eye(N), np.eye(N))))
+        h = cvxopt.matrix(np.hstack((np.zeros(N), self.C * np.ones(N))))
+        A = cvxopt.matrix(y.reshape(1, -1)) * 1.0
+        b = cvxopt.matrix(np.zeros(1))
+        # Solve the quadratic program using cvxopt       
+        cvxopt.solvers.options['show_progress'] = True
+        solution = cvxopt.solvers.qp(P, q, G, h, A, b)
+        # Lagrange multipliers
+        self.alpha = np.ravel(solution['x'])
         
-        # Constraints on alpha of the shape :
-        # -  d - C*alpha  = 0
-        # -  b - A*alpha >= 0
-        alpha = np.ones(N)
-        fun_eq = lambda alpha:  alpha@y # '''----------------function defining the equality constraint------------------'''        
-        jac_eq = lambda alpha:   y #'''----------------jacobian wrt alpha of the  equality constraint------------------'''
-        fun_ineq = lambda alpha:   np.hstack(((self.C - alpha),alpha)) # '''---------------function defining the inequality constraint-------------------'''     
-        jac_ineq = lambda alpha:   np.vstack((-np.eye(alpha.shape[0]),np.eye(alpha.shape[0])))# '''---------------jacobian wrt alpha of the  inequality constraint------------------- '''
-        constraints = ({'type': 'eq',  'fun': fun_eq, 'jac':jac_eq},
-                       {'type': 'ineq', 
-                        'fun': fun_ineq,
-                         'jac': jac_ineq  
-                        })
-
-        optRes = optimize.minimize(fun=lambda alpha: loss(alpha),
-                                   x0=np.zeros(N), 
-                                   method='SLSQP', 
-                                   jac=lambda alpha: grad_loss(alpha), 
-                                   constraints=constraints)
-        self.alpha = optRes.x
+        
         #clip
         self.alpha[self.alpha < 1e-5] = 0
         ## Assign the required attributes
         self.a = np.diag(y)@self.alpha 
         f = K_train@self.a
         mask = ((self.alpha < self.C) & (self.alpha > 0))
-        mask = (self.alpha < self.C)
-        self.b =  np.mean((1 - y[mask]*f[mask])/y[mask]) #''' -----------------offset of the classifier------------------ '''
+        self.b =  np.median((1 - y[mask]*f[mask])/y[mask]) #''' -----------------offset of the classifier------------------ '''
         self.norm_f = self.a.T@K_train@self.a   #'''------------------------RKHS norm of the function f ------------------------------'''
        
 
@@ -62,11 +59,17 @@ class KernelSVC:
     def separating_function(self, K_test):
         # Input : matrix x of shape N data points times d dimension
         # Output: vector of size N
+        K_test += 1
         return K_test@self.a
     
     
     def predict(self, K_test):
         """ Predict y values in {-1, 1} """
         d = self.separating_function(K_test)
-        return 2 * (d+self.b> 0) - 1
+        return 2 * ((d+self.b)> 0) - 1
+    
+    def predict_proba(self, X):
+        d = self.separating_function(X)
+        return sigmoid(d + self.b)
+    
     
--- utils.py
+++ utils.py
@@ -3,46 +3,6 @@ import networkx as nx
 
 def compute_product_graph(G1,G2):
     
-    '''
-    P = nx.tensor_product(G1, G2)
-    
-    #remove the nodes that they don't share same label
-    for n in list(P.nodes):
-        if(P.nodes[n]['labels'][0][0] != P.nodes[n]['labels'][1][0]):
-            P.remove_node(n)
-            
-    #remove the edges that they don't share same label        
-    for n in list(P.edges):
-        if(P.edges[n]['labels'][0][0] != P.edges[n]['labels'][1][0]):
-            P.remove_edge(n[0],n[1])
-  
-    if(len(list(P.nodes)) == 0):
-        return None
-    '''
-    
-    '''
-    P = nx.Graph()
-    nodes = []
-    for l in nx.product.product(G1, G2):
-        if (G1.nodes[l[0]]['labels'][0] == G2.nodes[l[1]]['labels'][0]):
-            nodes.append(l)
-    P.add_nodes_from(nodes)
-    
-    
-    
-    
-    
-    edges = []
-    for e1 in list(G1.edges):
-        for e2 in list(G2.edges):
-            a = e1[0]
-            b = e1[1]
-            c = e2[0]
-            d = e2[1]
-            if((G1.nodes[a]['labels'][0] == G2.nodes[c]['labels'][0]) and (G1.nodes[b]['labels'][0] == G2.nodes[d]['labels'][0]) and (G1.edges[e1]['labels'] == G2.edges[e2]['labels'])):
-                edges.append(((a,c),(b,d)))
-    P.add_edges_from(edges)     
-    ''' 
     P = nx.Graph()
     edges = []
     for l in G1.keys():
